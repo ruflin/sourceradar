bdb2741e7406076826edf701a6fdb2b98008ba51
Martin Naumann <martin.naumann@centralway.com>
2013-06-27 00:37:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sourceradar::Application.config.secret_token = 'cb4da6b89de55838fd4efd7108130053163dc4b6e4cbf38c6dee518cc044ff48aea1fc89355ea9d3e9167aec12188a868db4db51ec18baadb4f16db499897a35'
