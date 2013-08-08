0e2649e6d06bbb6cb8d763fd2206106c6ed9879d
Tania Pinheiro <tania.pinheiro@centralway.com>
2013-08-08 17:50:07 +0200

    GO DIE FOREVER, LANGUAGES - muarharharhar

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Sourceradar::Application.routes.draw do
   resources :rules
 
 
-  resources :languages
-
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
