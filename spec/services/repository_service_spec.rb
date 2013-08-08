4ab68ceba1295e36a94d6b2ae2c108c696f9e9c9
Tania Pinheiro <tania.pinheiro@centralway.com>
2013-08-08 17:43:40 +0200

    Refactored tests for RepositoryService

--- spec/services/repository_service_spec.rb
+++ spec/services/repository_service_spec.rb
@@ -1,10 +1,89 @@
-require 'rspec'
+require 'spec_helper'
 
 describe RepositoryService do
 
-  it 'should do something' do
+  let(:empty_url) { nil }
+  let(:invalid_url) { "https://github.com/centralway/whoopwhoop" }
+  let(:valid_url) { "https://github.com/centralway/sourceradar.git" }
+  let(:valid_branch) { "development" }
+  let(:invalid_branch) { "thisdoesntexistdude" }
 
-    #To change this template use File | Settings | File Templates.
-    true.should == false
+  describe "is_valid_url?" do
+
+    let(:repo_with_valid_url) { RepositoryService.new(valid_url, nil) }
+    let(:repo_with_invalid_url) { RepositoryService.new(invalid_url, nil) }
+
+    it "should return true on valid URL" do
+      @valid_response = double().as_null_object
+      @valid_response.should_receive(:code).and_return(200)
+      HTTParty.should_receive(:get).once.with(valid_url).and_return(@valid_response)
+      expect(repo_with_valid_url.is_valid_url?).to be_eql(true)
+    end
+
+    it "should return false on invalid URL" do
+      @invalid_response = double().as_null_object
+      @invalid_response.should_receive(:code).and_return(404)
+      HTTParty.should_receive(:get).once.with(invalid_url).and_return(@invalid_response)
+      expect(repo_with_invalid_url.is_valid_url?).to be_eql(false)
+    end
+
+  end
+
+  describe "branch_exists?" do
+
+    let(:repo_with_valid_branch) { RepositoryService.new(valid_url, valid_branch) }
+    let(:repo_with_invalid_branch) { RepositoryService.new(valid_url, invalid_branch) }
+    let(:repo_with_empty_branch) { RepositoryService.new(valid_url, nil) }
+
+    it "should return true on valid branch" do
+
+      branch_url = valid_url.chomp('.git') + '/branches/' + valid_branch
+
+      @valid_response = double().as_null_object
+      @valid_response.should_receive(:code).and_return(200)
+      HTTParty.should_receive(:get).once.with(branch_url).and_return(@valid_response)
+      expect(repo_with_valid_branch.branch_exists?).to be_eql(true)
+    end
+
+    it "should return false on invalid branch" do
+      branch_url = valid_url.chomp('.git') + '/branches/' + invalid_branch
+      @invalid_response = double().as_null_object
+      @invalid_response.should_receive(:code).and_return(404)
+      HTTParty.should_receive(:get).once.with(branch_url).and_return(@invalid_response)
+      expect(repo_with_invalid_branch.branch_exists?).to be_eql(false)
+    end
+
+    it "should return true on empty branch" do
+      branch_url = valid_url.chomp('.git') + '/branches/'
+      @valid_response = double().as_null_object
+      @valid_response.should_receive(:code).and_return(200)
+      HTTParty.should_receive(:get).once.with(branch_url).and_return(@valid_response)
+      expect(repo_with_empty_branch.branch_exists?).to be_eql(true)
+    end
+  end
+
+
+
+  context "crop should remove .git from URLs" do
+
+    let(:url_with_dot_git) { "blabla.git" }
+    let(:url_without_dot_git) { "blabla" }
+
+    subject { RepositoryService.new(url_with_dot_git,nil) }
+    specify do
+      expect(subject.chopUrl(url_with_dot_git)).to be_eql(url_without_dot_git)
+    end
+
+    subject { RepositoryService.new(url_without_dot_git,nil) }
+    specify do
+      expect(subject.chopUrl(url_without_dot_git)).to be_eql(url_without_dot_git)
+    end
+
+
+  end
+
+
+  it "should call the engine to analyze the repo" do
+    pending "The engine should be called when we're at that point"
   end
 end
