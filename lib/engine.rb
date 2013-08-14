29002eae66557725a2e8127dfad4ff95406bcf36
taniapinheiro <tania.pinheiro@centralway.com>
2013-08-14 14:46:22 +0200

    Created RepositoryReport

--- lib/engine.rb
+++ lib/engine.rb
@@ -1,5 +1,6 @@
 require_relative "engine/repository_cloner"
 require_relative "engine/repository_service"
+require_relative "engine/repository_report"
 
 module Engine
 end
---
+++ lib/engine/repository_report.rb
@@ -0,0 +1,6 @@
+module Engine
+  class RepositoryReport
+
+
+  end
+end
