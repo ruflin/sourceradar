99d327f5c40de0901ac1cdff08948783fbfc2a4f
Tania Pinheiro <tania.pinheiro@centralway.com>
2013-08-19 13:54:53 +0200

    Adding VulnerabilityImpact and Language to the Rule model

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,9 +1,11 @@
 class Rule < ActiveRecord::Base
-  belongs_to :language
+  has_one :language
+  has_one :vulnerability_impact
 
-  attr_accessible :expression, :businessnote, :technicalnote, :language_id
+  attr_accessible :expression, :businessnote, :technicalnote, :language_id, :vulnerability_impact_id
 
   validates :expression, presence: true, uniqueness: { scope: :language_id }
+  validates :vulnerability_impact_id, presence: true
   validates :businessnote, presence: true
   validates :technicalnote, presence: true
   validates :language_id, presence: true
