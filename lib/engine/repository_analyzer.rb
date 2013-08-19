f839d04249906048d42e2546d309729848d29e0e
Tania Pinheiro <tania.pinheiro@centralway.com>
2013-08-19 18:13:03 +0200

    Trying to make RepositoryAnalyzer work

--- lib/engine/repository_analyzer.rb
+++ lib/engine/repository_analyzer.rb
@@ -1,4 +1,5 @@
 require 'ripper'
+require 'json'
 
 module Engine
   class RepositoryAnalyzer
@@ -10,13 +11,14 @@ module Engine
       Dir.glob(@path + "/**/*")
     end
 
-    def get_ruby_files
+    def get_ruby_files(extension)
       dir_content = Dir.glob(@path + "/**/*")
       ruby_files = []
       dir_content.each do |file|
 
-        if file[-3,3] == ".rb"
+        if file[-extension.length-1,extension.length+1] == "."+extension
             ruby_files << file
+
         end
       end
       ruby_files
@@ -25,7 +27,27 @@ module Engine
     def analyze_ruby_file(file_name)
       tokens = Ripper.lex(File.read(file_name))
       tokens.delete_if { |token| token[1] != :on_ident }
-      tokens.map { |token| token[2] }
+     # tokens.map { |token| token[2] }
     end
+
+
+    def analyze_working_dir
+      ruby_files = get_ruby_files("rb") #Todo refactor this to other languages
+      result = {}
+      vuln = []
+
+      puts ruby_files.to_s
+      ruby_files.each do |file|
+          tokens = analyze_ruby_file(file)
+          tokens.each do |token|
+            vuln << {token: "#{token[2]}", line:token[0][0], column:token[0][1], file_name:file}
+          end
+      end
+    result = { project_name:"test_project", vulnerabilities: vuln }
+      htj = result.to_json
+    puts htj
+    htj
+    end
+
   end
 end
--- spec/lib/repository_analyzer_spec.rb
+++ spec/lib/repository_analyzer_spec.rb
@@ -1,10 +1,12 @@
 require 'rspec'
 require 'engine'
+require 'json'
 
 describe 'RepositoryAnalyzer' do
 
 
   let(:path) { "/some/path"}
+  let(:extension) {"rb"}
   let(:analyzer) { Engine::RepositoryAnalyzer.new(path) }
   let(:dir_content) { ['file1.rb', 'subdir/file2.rb', 'file3.txt'] }
   let(:dir_content_with_only_ruby_files) { ['file1.rb', 'subdir/file2.rb'] }
@@ -21,15 +23,64 @@ describe 'RepositoryAnalyzer' do
   describe "get_ruby_files" do
     it 'should only list .rb files' do
       Dir.stub(:glob).with(path+"/**/*").and_return(dir_content)
-      expect(analyzer.get_ruby_files).to be_eql(dir_content_with_only_ruby_files)
+      expect(analyzer.get_ruby_files(extension)).to be_eql(dir_content_with_only_ruby_files)
     end
   end
 
   describe "analyze_ruby_file" do
-    let(:result) { ["some_func", "result", "swapcase!", "puts", "result"] }
+    let(:result) { [[[1, 4], :on_ident, "some_func"], [[2, 2], :on_ident, "result"], [[2, 18], :on_ident, "swapcase!"], [[3, 2], :on_ident, "puts"], [[3, 7], :on_ident, "result"]] }
 
     it 'should return the tokens for ruby when analyzing a ruby file' do
       expect(analyzer.analyze_ruby_file("spec/fixtures/test_ripper.rb")).to be_eql(result)
+    end
+  end
+
+  describe "analyse working dir"  do
+
+    let(:dir_content) { ['spec/fixtures/test_ripper.rb'] }
+    let(:result) { ['spec/fixtures/test_ripper.rb', ["some_func", "result", "swapcase!", "puts", "result"]] }
+    let(:report) do
+      {
+        project_name: "test project",
+        vulnerabilities: [
+          {
+            token: "some_func",
+            line: 1,
+            column: 5,
+            file_name: "spec/fixtures/test_ripper.rb"
+          },
+          {
+              token: "result",
+              line: 2,
+              column: 2,
+              file_name: "spec/fixtures/test_ripper.rb"
+          },
+          {
+            token: "swapcase!",
+            line: 2,
+            column: 18,
+            file_name: "spec/fixtures/test_ripper.rb"
+          },
+          {
+              token: "puts",
+              line: 3,
+              column: 2,
+              file_name: "spec/fixtures/test_ripper.rb"
+          },
+          {
+              token: "result",
+              line: 3,
+              column: 7,
+              file_name: "spec/fixtures/test_ripper.rb"
+          },
+        ]
+      }
+    end
+
+    it "should return the tokens in JSON format " do
+      Dir.stub(:glob).with(path+"/**/*").and_return(dir_content)
+      result = puts report.to_json
+     expect(analyzer.analyze_working_dir).to be_eql(result)
     end
   end
 
