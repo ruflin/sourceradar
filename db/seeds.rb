be36d232e5423fed9c764248649dcdc561658ba7
Tania Pinheiro <tania.pinheiro@centralway.com>
2013-08-20 10:48:07 +0200

    Put some meat to the seed, yo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+rubyLanguage = Language.create(name:"Ruby",file_extension:"rb")
+
+ruleTypeFiltering = TypeOfRule.create(type:"Filtering")
+
+Rule.create(expression:"swapcase!", businessnote:"biz note", technicalnote:"tech note", language_id: rubyLanguage.id, type_of_rule_id: ruleTypeFiltering.id)
+Rule.create(expression:"like", businessnote:"biz note", technicalnote:"tech note", language_id: rubyLanguage.id, type_of_rule_id: ruleTypeFiltering.id)
